b5840a6ec4ad601615a457ea8ceccafcc02856d7
kinrezc <matt.czernik@gmail.com>
2024-12-23 11:31:02 -0500

    begin shadcn migration

--- packages/doppler-miniapp/vite.config.ts
+++ packages/doppler-miniapp/vite.config.ts
@@ -2,7 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
+const path = require("path");
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
