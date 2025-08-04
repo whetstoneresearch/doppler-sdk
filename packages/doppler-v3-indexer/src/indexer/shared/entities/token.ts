653e227e0220e5dac7b179fbfb22914f2b190a89
Rob Aparri <32496022+raparri01@users.noreply.github.com>
2025-08-04 12:56:04 -0400

    address format

--- packages/doppler-v3-indexer/src/indexer/shared/entities/token.ts
+++ packages/doppler-v3-indexer/src/indexer/shared/entities/token.ts
@@ -84,7 +84,7 @@ export const insertTokenIfNotExists = async ({
       ...multicallOptions,
     });
 
-    fetch(`${process.env.METADATA_UPDATER_ENDPOINT}?tokenAddress=${address.toLowerCase()}`) as unknown;
+    fetch(`${process.env.METADATA_UPDATER_ENDPOINT}?tokenAddress=${address}`) as unknown;
 
     return await context.db
       .insert(token)
