72164d0fb5ae8654ba5ea9f593b8d50bdfe0d9b7
matt <matt.czernik@gmail.com>
2025-09-11 17:07:22 -0400

    add base sep oracle, scaffold multicurve

---
+++ packages/doppler-v3-indexer/src/abis/multicurve-abis/index.ts
@@ -0,0 +1,3 @@
+export { UniswapV4MulticurveInitializerABI } from "./UniswapV4MulticurveInitializerABI";
+export { UniswapV4MulticurveInitializerHookABI } from "./UniswapV4MulticurveInitializerHookABI";
+export { UniswapV4MulticurveMigratorABI } from "./UniswapV4MulticurveMigratorABI";
