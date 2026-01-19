7eb2ad1ad38e9b4269c71146965660d884afbcda
Rusty <219724427+rustyshackleforddoteth@users.noreply.github.com>
2026-01-18 20:28:55 -0700

    refactor: split bytecodes.ts into individual files
    Split the large 186KB bytecodes.ts file into separate modules for better
    code organization and tree-shaking potential.

---
+++ src/common/abis/bytecodes/index.ts
@@ -0,0 +1,6 @@
+export { StateViewBytecode } from './stateViewBytecode';
+export { DopplerDN404Bytecode } from './dopplerDN404Bytecode';
+export { DopplerBytecode } from './dopplerBytecode';
+export { DopplerBytecodeBaseMainnet } from './dopplerBytecodeBaseMainnet';
+export { DERC20Bytecode } from './derc20Bytecode';
+export { DERC2080Bytecode } from './derc2080Bytecode';
